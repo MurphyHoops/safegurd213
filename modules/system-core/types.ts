3abc74ac41cadb23d4d405af34c83f395f7196eb
MurphyHoops <86187309+MurphyHoops@users.noreply.github.com>
2026-09-13 19:29:05 +0800

    cleanup: remove retired source-view prop

--- modules/system-core/types.ts
+++ modules/system-core/types.ts
@@ -5,7 +5,6 @@ export interface SystemCoreProps {
     settings: SystemSettings;
     onChange: (key: string, value: any) => void;
     onOpenManual: () => void;
-    onViewSource: () => void;
     onFactoryReset: () => void;
     onExportSettings: (name: string) => void;
     onImportSettings: (e: React.ChangeEvent<HTMLInputElement>) => void;
